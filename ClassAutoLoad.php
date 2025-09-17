cf1ba7ab11fd8d08673ea12adfa5902db95cdccc
SMosoti <sherly.mosoti@strathmore.edu>
2025-09-17 16:19:22 +0300

    first commit

--- ClassAutoLoad.php
+++ ClassAutoLoad.php
@@ -9,7 +9,7 @@ spl_autoload_register(function ($class_name) use ($directory) {
     foreach ($directory as $dir) {
         $filePath = __DIR__ . '/' . $dir . '/' . $class_name . '.php';
         if (file_exists($filePath)) {
-            require_once $file;
+            require_once $filePath;
             return;
         }
     }
