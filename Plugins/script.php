cb36a32256886872fac7067c426a30276261710d
SMosoti <sherly.mosoti@strathmore.edu>
2025-09-18 22:58:01 +0300

    db

--- Plugins/script.php
+++ Plugins/script.php
@@ -14,7 +14,7 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
-    echo "Connected to PostgreSQL successfully!";
+    echo "Connected to PostgreSQL successfully";
     
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
